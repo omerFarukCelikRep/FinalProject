abc21ca3e4f5e659caa352bf31105b0021199e0a
ofarukcelik <ofarukcelik@outlook.com>
2021-02-18 19:14:55 +0300

    ProductManager implemented from IProductService and constructor created with IProductDal

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,27 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        //Bir iş sınıfı başka sınıfı new'lemez
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //İş Kodları
+            return _productDal.GetAll();
+        }
+    }
+}
