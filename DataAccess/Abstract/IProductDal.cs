9cd8c0ddd5c3ba7c34f6147e3142e05d6d3e3327
ofarukcelik <ofarukcelik@outlook.com>
2021-02-18 18:31:44 +0300

    Product interface and methods created

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IProductDal
+    {
+        //Methods Default Acces Modifiers => Public
+        List<Product> GetAll();
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+    }
+}
