df40ea704b7e5c97c36673757bd5405633c3f42c
ofarukcelik <ofarukcelik@outlook.com>
2021-02-18 18:21:30 +0300

    IEntity interface created

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //IEntity implement eden Class bir veritabanı tablosudur
+    public interface IEntity
+    {
+    }
+}
