b2bae3c89c2c9eb9b7d03535bbf5be492fab38e1
ofarukcelik <ofarukcelik@outlook.com>
2021-02-20 19:20:06 +0300

    Database connection simulated

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,15 +1,18 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using System;
 
 namespace ConsoleUI
 {
+    //SOLID
+    //O => Open Closed Principle
     class Program
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+            ProductManager productManager = new ProductManager(new EfProductDal());
 
             foreach (Product product in productManager.GetAll())
             {
