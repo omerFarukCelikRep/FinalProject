d0d4e05a4ed3ae542703e8719a97efd3fd2bb3b8
ofarukcelik <ofarukcelik@outlook.com>
2021-02-20 19:35:02 +0300

    filtered list simulated

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -14,6 +14,31 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
+            //GetAllProducts(productManager);
+
+            //GetProductByCategoryId(productManager);
+
+            GetProductsByUnitPrice(productManager);
+        }
+
+        private static void GetProductsByUnitPrice(ProductManager productManager)
+        {
+            foreach (Product product in productManager.GetProductsByUnitPrice(50, 100))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+
+        private static void GetProductByCategoryId(ProductManager productManager)
+        {
+            foreach (Product product in productManager.GetAllByCategoryId(2))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+
+        private static void GetAllProducts(ProductManager productManager)
+        {
             foreach (Product product in productManager.GetAll())
             {
                 Console.WriteLine(product.ProductName);
