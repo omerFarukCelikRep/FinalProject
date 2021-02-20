63f34af62a855be1ee2ffa688fd3066566011542
ofarukcelik <ofarukcelik@outlook.com>
2021-02-20 17:51:40 +0300

    new() generic constraint added

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -10,7 +10,8 @@ namespace DataAccess.Abstract
     //Generic Constraint => Generic Kısıt
     // class => referans tip olabilir 
     //IEntity  => IEntity olabilir yada IEntity implemente eden bir nesne olabilir
-    public interface IEntityRepository<T> where T : class, IEntity
+    //new() => new 'lenebilir olmalı
+    public interface IEntityRepository<T> where T : class, IEntity,new()
     {
         List<T> GetAll(Expression<Func<T, bool>> filter = null);
         T Get(Expression<Func<T, bool>> filter);
