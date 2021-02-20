7cc644791254088f682d95580ab02386029b6334
ofarukcelik <ofarukcelik@outlook.com>
2021-02-20 17:49:17 +0300

    T type contstrained as a IEntity

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
@@ -8,7 +9,8 @@ namespace DataAccess.Abstract
     //Generic Repository Design Pattern
     //Generic Constraint => Generic Kısıt
     // class => referans tip olabilir 
-    public interface IEntityRepository<T> where T : class
+    //IEntity  => IEntity olabilir yada IEntity implemente eden bir nesne olabilir
+    public interface IEntityRepository<T> where T : class, IEntity
     {
         List<T> GetAll(Expression<Func<T, bool>> filter = null);
         T Get(Expression<Func<T, bool>> filter);
