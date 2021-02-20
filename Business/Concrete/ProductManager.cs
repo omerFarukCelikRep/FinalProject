6214daa29df13e503176d0030ea6cf7b3fbe4acf
ofarukcelik <ofarukcelik@outlook.com>
2021-02-20 19:34:12 +0300

    new methods added and implemented

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -23,5 +23,15 @@ namespace Business.Concrete
             //İş Kodları
             return _productDal.GetAll();
         }
+
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            return _productDal.GetAll(p => p.CategoryID == id);
+        }
+
+        public List<Product> GetProductsByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
+        }
     }
 }
