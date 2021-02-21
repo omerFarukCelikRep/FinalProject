7e5442578503626d830be85ec6e0a7a22a2467e0
ofarukcelik <ofarukcelik@outlook.com>
2021-02-21 23:47:56 +0300

    Category Business class created

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDal _catgoryDal;
+
+        public CategoryManager(ICategoryDal catgoryDal)
+        {
+            _catgoryDal = catgoryDal;
+        }
+
+        public List<Category> GetAll()
+        {
+            return _catgoryDal.GetAll();
+        }
+
+        public Category GetById(int categoryId)
+        {
+            return _catgoryDal.Get(c => c.CategoryID == categoryId);
+        }
+    }
+}
