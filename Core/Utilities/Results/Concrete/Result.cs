b50461bbdc6be1f6dd56003585ffda99e9b2e903
ofarukcelik <ofarukcelik@outlook.com>
2021-02-24 20:11:02 +0300

    Result constructors created

--- Core/Utilities/Results/Concrete/Result.cs
+++ Core/Utilities/Results/Concrete/Result.cs
@@ -7,6 +7,15 @@ namespace Core.Utilities.Results.Concrete
 {
     public class Result : IResult
     {
+        public Result(bool success, string message) : this(success)
+        {
+            this.Message = message;
+        }
+        public Result(bool success)
+        {
+            this.Success = success;
+        }
+
         public bool Success { get; }
 
         public string Message { get; }
