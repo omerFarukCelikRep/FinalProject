c4305468822f0e21ef7b6418e83e50545135f284
ofarukcelik <ofarukcelik@outlook.com>
2021-02-25 19:36:23 +0300

    Static Message class created

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -7,6 +7,11 @@ namespace Business.Constants
     public static class Messages
     {
         public static string ProductAdded = "Product Added";
+
         public static string ProductNameInvalid = "Product Name is not valid";
+
+        public static string MainteanceTime = "System Mainteance";
+
+        public static string ProductsListed = "Products Listed";
     }
 }
