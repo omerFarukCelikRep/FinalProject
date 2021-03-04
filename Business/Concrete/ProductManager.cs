660f8f75344713b93ac58851e12ee8085bd5fe15
ofarukcelik <ofarukcelik@outlook.com>
2021-03-04 19:17:15 +0300

    condition changed

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -38,7 +38,7 @@ namespace Business.Concrete
         public IDataResult<List<Product>> GetAll()
         {
             //İş Kodları
-            if (DateTime.Now.Hour == 22)
+            if (DateTime.Now.Hour == 18)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MainteanceTime);
             }
